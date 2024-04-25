4730f50beae7d2f79b972c0d060cb57f9e14cf7e
Lemo Trano <tran1219@umn.edu>
2024-04-25 15:43:20 -0500

    added changes to the education desc

--- src/components/Education/index.js
+++ src/components/Education/index.js
@@ -82,9 +82,6 @@ const index = () => {
         <Container id="education">
             <Wrapper>
                 <Title>Education</Title>
-                <Desc>
-                    My education has been a journey of self-discovery and growth. My educational details are as follows.
-                </Desc>
                 <TimelineSection>
                     <Timeline>
                         {education.map((education,index) => (
